627509768cdd508209b5b410b78341bf7190791c
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-05-30 17:14:36 +0300

    Added Slider

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,11 @@ const App = () => {
   const [name, setName] = useState('');
   const [topic, setTopic] = useState('');
   const [selectedFile, setSelectedFile] = useState(null);
-
+  const [x, setX] = useState(130);
+  const [y, setY] = useState(100);
   const [stamped, setStamped] = useState('');
+
+  const [jpgBytes, setJpgBytes] = useState('');
 
   const cleanUp = () => {
     setName('');
@@ -31,15 +34,16 @@ const App = () => {
 
     const jpgUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&format=jpg&data=${name} ${topic} date: ${new Date()}`;
 
-    const jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer());
+    if (!jpgBytes)
+      setJpgBytes(await fetch(jpgUrl).then((res) => res.arrayBuffer()));
 
-    const jpgImage = await pdfDoc.embedJpg(jpgImageBytes);
+    const jpgImage = await pdfDoc.embedJpg(jpgBytes);
 
     const jpgDims = jpgImage.scale(0.75);
 
     page.drawImage(jpgImage, {
-      x: 130,
-      y: 100,
+      x,
+      y,
       width: jpgDims.width,
       height: jpgDims.height,
     });
@@ -57,6 +61,7 @@ const App = () => {
     fileDownload(stamped, `Certificate for ${name}.pdf`);
     cleanUp();
   };
+
   return (
     <>
       <div style={{ display: 'flex', flexDirection: 'column', gap: '3rem' }}>
@@ -71,17 +76,47 @@ const App = () => {
           onChange={(e) => setTopic(e.target.value)}
         />
         <input type="file" accept=".pdf" onChange={handleFileChange} />
+
         {stamped && (
-          <object
-            data={URL.createObjectURL(
-              new Blob([stamped], { type: 'application/pdf' })
-            )}
-            type="application/pdf"
-            width="100%"
-            height="100%"
-          >
-            <p>PDF preview is not available</p>
-          </object>
+          <>
+            <div className="slider">
+              <input
+                className="vertical-slider"
+                type="range"
+                min="0"
+                max="500"
+                value={x}
+                onChange={(e) => {
+                  setX(Number(e.target.value));
+                  modifyPdf();
+                }}
+              />
+              <div>
+                <object
+                  className="preview"
+                  data={URL.createObjectURL(
+                    new Blob([stamped], { type: 'application/pdf' })
+                  )}
+                  type="application/pdf"
+                  width="100%"
+                  height="100%"
+                >
+                  <p>PDF preview is not available</p>
+                </object>
+                <input
+                  className="horizontal-slider"
+                  type="range"
+                  min="0"
+                  max="500"
+                  value={y}
+                  onChange={(e) => {
+                    setY(Number(e.target.value));
+                    modifyPdf();
+                  }}
+                />
+              </div>
+            </div>
+          </>
         )}
         {!stamped ? (
           <button onClick={() => modifyPdf()}>Preview PDF</button>
